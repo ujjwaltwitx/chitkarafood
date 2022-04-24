5313854e1da3526747bad09391793abb8ad0871a
ujjwaltwitx <ujjwalcpj123@gmail.com>
2022-04-24 13:22:18 +0530

    final commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,25 @@
-import './App.css';
 import Header from './components/header';
+<<<<<<< HEAD
 // import Card from './components/card';
 import UploadForm from './components/form';
+=======
+import Card from './components/card';
+import SubHeader from './components/subHeader';
+
+>>>>>>> 79ff1770a983932ad38149c901db4af830c01218
 function App() {
   return (
     <div>
       <Header />
+<<<<<<< HEAD
       <UploadForm />
       <footer id='footer'>
         After uploading your mess card, If any sutdent buys it, he or she will contact you for your mess card. We don't take any responsibility for the same but, as a human being, we suppose that you know how it feels to be hungry and cheated at the same time. So, we request you to be honest with the information that you provide.
       </footer>
+=======
+      <SubHeader />
+      <Card />
+>>>>>>> 79ff1770a983932ad38149c901db4af830c01218
     </div>
   );
 }
---
+++ src/components/subHeader.jsx
@@ -0,0 +1,22 @@
+import '../styles/title.css'
+
+const SubHeader = () => {
+    return ( 
+        <div className="subHeader">
+            <div className="hostelTitle">
+                Hostel Name
+            </div>
+            <div className="numbTitle">
+                Card Number
+            </div>
+            <div className="priceTitle">
+                Price
+            </div>
+            <div className="paymentTitle">
+                Payment
+            </div>
+        </div>
+     );
+}
+ 
+export default SubHeader;
