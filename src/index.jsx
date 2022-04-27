e53a0b7d1343585033a3a9dd0b1c8ee8bced4cbb
AngerAstra <udhavwadhawan@hotmail.com>
2022-04-27 11:32:56 +0530

    initialized fetching data

--- src/index.jsx
+++ src/index.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { render } from '@testing-library/react';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -10,5 +11,3 @@ root.render(
     <App />
   </React.StrictMode>
 );
-
-
